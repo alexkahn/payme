cea079431d70e893c75fdaed710dc25cd38a9f0e
Alex Kahn <alex.kahn@outlook.com>
2016-03-28 23:31:18 -0500

    initial commit

---
+++ routes/transactions.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const schema = require('../lib/cardSchema');
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/', function(req, res, next) {
+  schema.validate(req.body, function(err, value) {
+    if (err) {
+      console.log(err);
+      return res.send(err);
+    }
+    return res.send('respond with a resource');
+  })
+});
+
+module.exports = router;
